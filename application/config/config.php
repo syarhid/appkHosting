1b7938ef1c3efd08ab8175fde80ac2072eaf110b
syarhid <71324290+syarhid@users.noreply.github.com>
2020-09-15 18:49:43 +0700

    15 sep 2020

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/appk/';
+$config['base_url'] = 'http://appk.rumah-halal.com/appk/';
 
 /*
 |--------------------------------------------------------------------------
