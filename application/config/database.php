5263d71a57b6f4f59cdd1b3daa82e9aca13d3219
syarhid <71324290+syarhid@users.noreply.github.com>
2020-09-15 22:13:18 +0700

    15 sep 20

--- application/config/database.php
+++ application/config/database.php
@@ -73,24 +73,49 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'u455978133_appk',
-	'password' => '&0pV4Hj~XrV',
-	'database' => 'u455978133_appk',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
+if ($_SERVER['HTTP_HOST'] == "localhost") {
+	$db['default'] = array(
+	  'dsn'	=> '',
+	  'hostname' => 'localhost',
+	  'username' => 'root',
+	  'password' => '',
+	  'database' => 'appk',
+	  'dbdriver' => 'mysqli',
+	  'dbprefix' => '',
+	  'pconnect' => FALSE,
+	  'db_debug' => (ENVIRONMENT !== 'production'),
+	  'cache_on' => FALSE,
+	  'cachedir' => '',
+	  'char_set' => 'utf8',
+	  'dbcollat' => 'utf8_general_ci',
+	  'swap_pre' => '',
+	  'encrypt' => FALSE,
+	  'compress' => FALSE,
+	  'stricton' => FALSE,
+	  'failover' => array(),
+	  'save_queries' => TRUE
+  ); 
+	  
+  }else {
+	$db['default'] = array(
+	  'dsn'	=> '',
+	  'hostname' => 'localhost',
+	  'username' => 'u455978133_appk',
+	  'password' => '&0pV4Hj~XrV',
+	  'database' => 'u455978133_appk',
+	  'dbdriver' => 'mysqli',
+	  'dbprefix' => '',
+	  'pconnect' => FALSE,
+	  'db_debug' => (ENVIRONMENT !== 'production'),
+	  'cache_on' => FALSE,
+	  'cachedir' => '',
+	  'char_set' => 'utf8',
+	  'dbcollat' => 'utf8_general_ci',
+	  'swap_pre' => '',
+	  'encrypt' => FALSE,
+	  'compress' => FALSE,
+	  'stricton' => FALSE,
+	  'failover' => array(),
+	  'save_queries' => TRUE
+  );
+}
